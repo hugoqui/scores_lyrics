583772040c3f479f9b57e3d407ca8c240146551d
hugoqui <hugo_2555@hotmail.com>
2025-08-09 19:38:29 -0600

    correcion colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{css,xml,html,vue,svelte,ts,tsx}'],
   // use the .ns-dark class to control dark mode (applied by NativeScript) - since 'media' (default) is not supported.
-  darkMode: ['class', '.ns-dark'],
+  // darkMode: ['class', '.ns-dark'],
   theme: {
     extend: {},
   },
