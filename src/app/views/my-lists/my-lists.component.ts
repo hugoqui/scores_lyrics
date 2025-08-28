63675b15e3853204161469bc4ca32337d9d4beda
hugoqui <hugo_2555@hotmail.com>
2025-08-28 09:15:55 -0600

    listview sin swipe

--- src/app/views/my-lists/my-lists.component.ts
+++ src/app/views/my-lists/my-lists.component.ts
@@ -5,7 +5,7 @@ import { SongList } from '../../models/songList'
 import { SongListsService } from '../../services/song-lists.service'
 import { prompt, confirm } from "@nativescript/core/ui/dialogs";
 import { CollectionViewModule } from '@nativescript-community/ui-collectionview/angular';
-import {install} from '@nativescript-community/ui-collectionview-swipemenu';
+import { install } from '@nativescript-community/ui-collectionview-swipemenu';
 install();
 
 @Component({
@@ -13,7 +13,7 @@ install();
   selector: 'ns-my-lists',
   templateUrl: 'my-lists.component.html',
   styleUrls: ['my-lists.component.css'],
-  imports: [NativeScriptCommonModule, NativeScriptRouterModule, CollectionViewModule ],
+  imports: [NativeScriptCommonModule, NativeScriptRouterModule, CollectionViewModule],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class MyListsComponent {
@@ -59,5 +59,22 @@ export class MyListsComponent {
       true
     }
   }
+
+  items = [
+    { index: 0, name: 'TURQUOISE', color: '#1abc9c' },
+    { index: 1, name: 'EMERALD', color: '#2ecc71' },
+    { index: 2, name: 'PETER RIVER', color: '#3498db' },
+    { index: 3, name: 'AMETHYST', color: '#9b59b6' },
+    { index: 4, name: 'WET ASPHALT', color: '#34495e' },
+    { index: 5, name: 'GREEN SEA', color: '#16a085' },
+    { index: 6, name: 'NEPHRITIS', color: '#27ae60' },
+    { index: 7, name: 'BELIZE HOLE', color: '#2980b9' },
+    { index: 8, name: 'WISTERIA', color: '#8e44ad' },
+    { index: 9, name: 'MIDNIGHT BLUE', color: '#2c3e50' }
+  ];
+  removeItem(i: number) {
+    this.items.splice(i, 1);
+  }
+
 }
 
