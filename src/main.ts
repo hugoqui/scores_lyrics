0b4389f98881daa0ae124f9e3770a082f3a63d82
hugo <hugo2555@gmail.com>
2025-09-07 21:49:38 -0600

    update version

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import {
   provideNativeScriptHttpClient,
   provideNativeScriptRouter,
   runNativeScriptAngularApp,
-  NSRouteReuseStrategy,
 } from '@nativescript/angular';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { RouteReuseStrategy } from '@angular/router';
 
 runNativeScriptAngularApp({
   appModuleBootstrap: () => {
@@ -18,7 +16,6 @@ runNativeScriptAngularApp({
         provideNativeScriptHttpClient(withInterceptorsFromDi()),
         provideNativeScriptRouter(routes),
         provideZonelessChangeDetection(),
-        { provide: RouteReuseStrategy, useClass: NSRouteReuseStrategy },
       ],
     });
   },
