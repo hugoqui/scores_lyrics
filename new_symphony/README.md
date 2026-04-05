b5bc4e80672fd09f2331307a3babaa6bed308343
hugoqui <34322132+hugoqui@users.noreply.github.com>
2026-04-05 16:52:10 -0600

    build info

--- new_symphony/README.md
+++ new_symphony/README.md
@@ -18,3 +18,4 @@ samples, guidance on mobile development, and a full API reference.
 
 # Build
 flutter build ipa --release
+flutter build appbundle --release
--- new_symphony/lib/main.dart
+++ new_symphony/lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Symphony',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
